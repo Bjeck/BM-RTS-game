8a4c915ace4e05d6337a5b38d8566f805ace08a3
Maretty21 <maretty21@gmail.com>
2014-06-26 11:30:20 +0200

    random shit

---
+++ MapTest/MappingTest/Assets/Scripts/ClickMarker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickMarker : MonoBehaviour {
+
+	public Camera camera;
+	public GameObject mark;
+	public bool startFollow = false;
+	private Vector3 startPos = new Vector3( 0.0f, 0.0f, -4.0f);
+
+	// Use this for initialization
+	void Start () {
+		mark.transform.position = startPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Ray ray = camera.ScreenPointToRay(new Vector3(200, 200, 0));
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+	}
+
+}
