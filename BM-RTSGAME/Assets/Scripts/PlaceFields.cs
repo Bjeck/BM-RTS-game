2b9b190353bb34e77f77c17399ca97e953310ea3
Maretty21 <maretty21@gmail.com>
2014-06-27 11:43:57 +0200

    Finally dynamic player spawn - they cannot overlap

--- BM-RTSGAME/Assets/Scripts/PlaceFields.cs
+++ BM-RTSGAME/Assets/Scripts/PlaceFields.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class PlaceFields : MonoBehaviour {
 	
@@ -8,21 +10,43 @@ public class PlaceFields : MonoBehaviour {
 	public Vector2 FieldDimensions;
 	public Vector3 fieldPosition;
 
-
-
 	// Use this for initialization
 	void Start () {
 	
-		int[,] PlayerArray = new int[player.Length, 3];
+		List<int> playerCoordinates = new List<int>();
 
-		for (int RunTimes = 0; RunTimes < player.Length; RunTimes++){
+		//------------------ Random location but not outer field. 
+		for (int AllPlayersGetCoordinates = 0; AllPlayersGetCoordinates < player.Length; AllPlayersGetCoordinates++){
 
-			PlayerArray[RunTimes,0] = RunTimes;
-			PlayerArray[RunTimes,1] = 1+(int)Random.Range(0, FieldDimensions[0]-2);
-			PlayerArray[RunTimes,2] = 1+(int)Random.Range(0, FieldDimensions[1]-2);
 
-			Debug.Log (PlayerArray[RunTimes,1]);
-			Debug.Log (PlayerArray[RunTimes,2]);
+
+			//--------------------------------------- Create lists
+			List<int> checklistX = new List<int>();
+			List<int> checklistY = new List<int>();
+
+			//--------------------------------------- Put Number into checklist
+			checklistX.Add(1+(int)Random.Range(0, FieldDimensions[0]-2));
+			checklistY.Add(1+(int)Random.Range(0, FieldDimensions[1]-2));
+
+			//--------------------------------------- Are numbers in the playerCoordinates?
+			bool XisClear = playerCoordinates.Any(item => checklistX.Contains(item));
+			bool YisClear = playerCoordinates.Any(item => checklistY.Contains(item));
+
+
+			//--------------------------------------- If they are, add it to the playerCoordinates list
+			if (!XisClear && !YisClear){
+				Debug.Log (checklistX[0]);
+				Debug.Log (checklistY[0]);
+				playerCoordinates.Add(checklistX[0]);
+				playerCoordinates.Add(checklistY[0]);
+			}
+			else{
+				AllPlayersGetCoordinates--;
+			}
+
+			//--------------------------------------- Clear the checklist
+			checklistX.Clear();
+			checklistY.Clear();
 		}
 
 		for (int i = 0; i < (int)FieldDimensions[0]; i += 1){
@@ -40,16 +64,25 @@ public class PlaceFields : MonoBehaviour {
 				float setYPos = setYPosArr + setPosMiddleY + setOriginY;
 				float setZPos = setOriginZ;
 
-				for (int CheckIfPlayerPos = 0; CheckIfPlayerPos < player.Length; CheckIfPlayerPos++){
-
-					if (i == PlayerArray[CheckIfPlayerPos,1] && j == PlayerArray[CheckIfPlayerPos,2]) {
-
+				//------------------------- CHECK IF PLAYER
+				bool itsAPlayer = false;
+				int runTimes = 0;
+				int runThroughPlayers = 0;
+				while (runTimes < player.Length){
+					if(i == playerCoordinates[runThroughPlayers] && j == playerCoordinates[runThroughPlayers+1]){
+						itsAPlayer = true; 
+						Instantiate (player[runTimes], new Vector3(setXPos, setYPos, setZPos), Quaternion.identity);
 					}
 
-					else{
-						Instantiate (StdFields, new Vector3(setXPos, setYPos, setZPos), Quaternion.identity);
-					}
+					runThroughPlayers+=2;
+					runTimes++;
 				}
+
+				//------------------------- IF NO PLAYER IS PLACED
+				if (itsAPlayer == false){
+					Instantiate (StdFields, new Vector3(setXPos, setYPos, setZPos), Quaternion.identity);	
+				}
+
 			}
 		}
 	}
