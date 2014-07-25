93fda16298b459def54806a8d8c2f11ddab248b5
Maretty21 <maretty21@gmail.com>
2014-07-25 15:13:36 +0200

    Resources fixed again, and obsticles now work agia

--- BM-RTSGAME/Assets/PlaceResource.cs
+++ BM-RTSGAME/Assets/PlaceResource.cs
@@ -6,10 +6,15 @@ public class PlaceResource : MonoBehaviour {
 	public GameObject Resource;
 	public Vector3 ResourcePosition;
 
+	private GameObject Astar;
+
 	// Use this for initialization
-	void Start () {
+	void Awake()
+	{
 		Instantiate (Resource, transform.position+ResourcePosition, Quaternion.identity);
 
+		//Astar = GameObject.Find ("A*");
+		//Astar.GetComponent<AstarPath>().Scan();
 	}
 	
 	// Update is called once per frame
