6df6c05c0919f7a1338dc47b02ff8da9fe70a9fe
Maretty21 <maretty21@gmail.com>
2014-07-26 12:47:45 +0200

    Quick menu
    In order to be able to work with networking later on next week.

---
+++ BM-RTSGAME/Assets/Scripts/Menu/NewGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+	private bool MouseIsHovering = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (MouseIsHovering){
+			transform.localScale = new Vector3(1.1f,1.1f,0.0f);
+		}else{
+			transform.localScale = new Vector3(1.0f,1.0f,0.0f);
+		}
+
+		if(Input.GetMouseButtonDown(0) && MouseIsHovering){
+			Application.LoadLevel("FieldConstruction");
+		}
+	}
+
+	void OnMouseOver(){
+		MouseIsHovering = true;
+	}
+	void OnMouseExit(){
+		MouseIsHovering = false;
+	}
+}
---
+++ BM-RTSGAME/Assets/Scripts/Menu/Quit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour {
+	
+	private bool MouseIsHovering = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (MouseIsHovering){
+			transform.localScale = new Vector3(1.1f,1.1f,0.0f);
+		}else{
+			transform.localScale = new Vector3(1.0f,1.0f,0.0f);
+		}
+		
+	}
+	
+	void OnMouseOver(){
+		MouseIsHovering = true;
+	}
+	void OnMouseExit(){
+		MouseIsHovering = false;
+	}
+}
