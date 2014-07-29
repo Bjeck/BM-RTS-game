c67988c3163fa2f59e313ad6aa46df1bd9f9c380
Toretty <MarettyBoogie@Madss-MacBook-Pro.local>
2014-07-29 22:54:16 +0200

    Test of Basic Network Connection

---
+++ BM-RTSGAME/Assets/Scripts/Network/NetworkManagerScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManagerScript : MonoBehaviour {
+	
+	private const string typeName = "Electro";
+	private const string gameName = "TestGame";
+
+	public GameObject playerPrefab;
+
+	private void StartServer()
+	{
+		// Initializes server - MAX_AMOUNT_OF_PLAYERS, PORT_NUMBER
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		
+		// Register game at master server
+		MasterServer.RegisterHost(typeName, gameName);
+
+	}
+	
+	/// <summary>
+	/// Prints message if server is initialized.
+	/// </summary>
+	void OnServerInitialized() {	
+		Debug.Log("Server Initializied");	
+		SpawnPlayer();
+	}
+
+	// List of hosts
+	private HostData[] hostList;
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Debug.Log("Joining Server - NonHost");
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Server Joined");
+		SpawnPlayer();
+	}
+
+	
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(playerPrefab, new Vector3(0.0f, 5.0f, 0.0f), Quaternion.identity, 0);
+	}
+	
+	void OnGUI()
+	{
+
+		//-------------------- JOIN
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+					JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+
+
+	
+
+}
---
+++ BM-RTSGAME/Assets/Scripts/Network/PlayerTestMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTestMovement : MonoBehaviour {
+
+		public float speed = 10f;
+		
+		void Update()
+		{
+			// Makes sure that no other player can use the same controls.
+			if (networkView.isMine)
+			{
+				InputMovement();
+			}
+		}
+		
+		void InputMovement()
+		{	
+				// Just simple character control
+				if (Input.GetKey (KeyCode.W)) {
+					transform.position = Vector3.Lerp (transform.position, new Vector3 (transform.position.x, transform.position.y + speed, transform.position.z), Time.deltaTime * 5);
+				}
+
+				if (Input.GetKey (KeyCode.D)) {
+					transform.position = Vector3.Lerp (transform.position, new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z), Time.deltaTime * 5);
+				}
+
+				if (Input.GetKey (KeyCode.A)) {
+						transform.position = Vector3.Lerp (transform.position, new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z), Time.deltaTime * 5);
+				}
+		}
+}
