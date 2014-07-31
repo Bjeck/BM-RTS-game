d612340037903da8884eec928b47c2ddf77e397c
Toretty <MarettyBoogie@Madss-MacBook-Pro.local>
2014-07-31 15:44:54 +0200

    Networking+StateSync+Interpolation Test

--- BM-RTSGAME/Assets/Scripts/Network/PlayerTestMovement.cs
+++ BM-RTSGAME/Assets/Scripts/Network/PlayerTestMovement.cs
@@ -3,30 +3,69 @@ using System.Collections;
 
 public class PlayerTestMovement : MonoBehaviour {
 
-		public float speed = 10f;
-		
-		void Update()
+	public float speed = 10f;
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector2 syncStartPosition = Vector2.zero;
+	private Vector2 syncEndPosition = Vector2.zero;
+
+
+	void OnSerializeNetworkView (BitStream stream, NetworkMessageInfo info){
+
+		Vector3 syncPosition = Vector3.zero;
+
+		if (stream.isWriting) {
+
+			syncPosition = transform.position;
+			stream.Serialize(ref syncPosition);
+
+		} else {
+			stream.Serialize(ref syncPosition);
+
+			syncTime = 0.0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+
+			Vector2 convertTo2D = new Vector2(syncPosition.x, syncPosition.y);
+			syncStartPosition = transform.position;
+			syncEndPosition = convertTo2D;
+		}
+	}
+
+
+	void Update()
+	{
+		// Makes sure that no other player can use the same controls.
+		if (networkView.isMine)
 		{
-			// Makes sure that no other player can use the same controls.
-			if (networkView.isMine)
-			{
-				InputMovement();
+			InputMovement();
+		} else {
+			SyncedMovement();
+		}
+	}
+	
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		transform.position = Vector2.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+	
+	void InputMovement()
+	{	
+			// Just simple character control
+			if (Input.GetKey (KeyCode.W)) {
+				transform.position = Vector2.Lerp (transform.position, new Vector2 (transform.position.x, transform.position.y + speed), Time.deltaTime * 5);
 			}
-		}
-		
-		void InputMovement()
-		{	
-				// Just simple character control
-				if (Input.GetKey (KeyCode.W)) {
-					transform.position = Vector3.Lerp (transform.position, new Vector3 (transform.position.x, transform.position.y + speed, transform.position.z), Time.deltaTime * 5);
-				}
 
-				if (Input.GetKey (KeyCode.D)) {
-					transform.position = Vector3.Lerp (transform.position, new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z), Time.deltaTime * 5);
-				}
+			if (Input.GetKey (KeyCode.D)) {
+				transform.position = Vector2.Lerp (transform.position, new Vector2 (transform.position.x + speed, transform.position.y), Time.deltaTime * 5);
+			}
 
-				if (Input.GetKey (KeyCode.A)) {
-						transform.position = Vector3.Lerp (transform.position, new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z), Time.deltaTime * 5);
-				}
-		}
+			if (Input.GetKey (KeyCode.A)) {
+					transform.position = Vector2.Lerp (transform.position, new Vector2 (transform.position.x - speed, transform.position.y), Time.deltaTime * 5);
+			}
+	}
+
+
 }
