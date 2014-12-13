4c5f1a82439886409149769093ab1051a7163fdf
Toretty <maretty21@gmail.com>
2014-12-13 19:29:54 +0100

    Interpolation + prediction unit networking

--- BM-RTSGAME/Assets/SyncUnitScript.cs
+++ BM-RTSGAME/Assets/SyncUnitScript.cs
@@ -5,29 +5,55 @@ using System.Collections;
 
 public class SyncUnitScript : MonoBehaviour {
 	
-	private Vector3 ReferencePosition;
-	
+	private Vector3 EndPosition = new Vector3(0.0f,0.0f,0.0f); 
+
+	private float syncDelay = 0.0f;
+	private float syncTime = 0.0f;
+	private float LastSyncTime = 0.0f;
+
 	void OnSerializeNetworkView (BitStream stream, NetworkMessageInfo info){
-		
+
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
 		// If this script is the one sending something.
 		if (stream.isWriting) {
 			
 			// Make a reference of the position right now.
-			ReferencePosition = transform.position;
+			syncPosition = transform.position;
 			
 			// Send it through the stream.
-			stream.Serialize (ref ReferencePosition);
-			
+			stream.Serialize (ref syncPosition);
+		
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
+
 		} 
 		// If the script is the one receiving.
 		else {
 			
 			// Receive the message from the sender. 
-			stream.Serialize (ref ReferencePosition);
-			
-			// Assigns the value to this object's position. 
-			transform.position = ReferencePosition;
-			
+			stream.Serialize (ref syncPosition);
+			stream.Serialize(ref syncVelocity);
+
+			syncTime = 0.0f;
+			syncDelay = Time.time - LastSyncTime;
+			LastSyncTime = Time.time;
+
+			EndPosition = syncPosition + syncVelocity * syncDelay;
 		}
 	}
+
+	void Update(){
+		if(!networkView.isMine) {
+			SyncMovement();		
+		}
+	}
+
+	private void SyncMovement(){
+
+		syncTime += Time.deltaTime;
+		transform.position = Vector3.Lerp (transform.position, EndPosition, syncTime / syncDelay);
+	
+	}
+
 }
